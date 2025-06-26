0e5162e3573afffdbdc3c1c7289c22ceffb86b91
CV JAGADEESHWAR <187868570+Jagadesh-1811@users.noreply.github.com>
2025-06-26 16:32:38 +0530

    Add files via upload

---
+++ so.py
@@ -0,0 +1,12 @@
+import math
+n=int(input('enter the number:'))
+def sqrt(n):
+    return math.sqrt(n)
+print('square root of', n, 'is', sqrt(n))
+def log(n):
+    return math.log(n)
+print('logrithm of ',n,'is',log(n))
+def sin(n):
+    return math.sin(n)
+print('sine of ',n,'is',sin(n))
+    
